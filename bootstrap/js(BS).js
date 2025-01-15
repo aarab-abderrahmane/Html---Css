0d4fe0c33fede5814e8364ac28bb789d09e7a268
aarab-abderrahmane <abdulrahman.tie@gmail.com>
2025-01-15 11:03:50 +0100

    Modification No.4,1

--- bootstrap/js(BS).js
+++ bootstrap/js(BS).js
@@ -1,10 +1,10 @@
-// all-bootstrap.js
+// // all-bootstrap.js
 
-// Import Bootstrap bundle files
-import './bootstrap.bundle.js';
-import './bootstrap.bundle.js.map';
-import './bootstrap.bundle.min.js';
-import './bootstrap.bundle.min.js.map';
+// // Import Bootstrap bundle files
+// import './bootstrap.bundle.js';
+// import './bootstrap.bundle.js.map';
+// import './bootstrap.bundle.min.js';
+// import './bootstrap.bundle.min.js.map';
 
 // Import Bootstrap ESM files
 import './bootstrap.esm.js';
@@ -12,8 +12,16 @@ import './bootstrap.esm.js.map';
 import './bootstrap.esm.min.js';
 import './bootstrap.esm.min.js.map';
 
-// Import Bootstrap core files
-import './bootstrap.js';
-import './bootstrap.js.map';
-import './bootstrap.min.js';
-import './bootstrap.min.js.map';
+// // Import Bootstrap core files
+// import './bootstrap.js';
+// import './bootstrap.js.map';
+// import './bootstrap.min.js';
+// import './bootstrap.min.js.map';
+
+// import './bootstrap.bundle.min.js';
+// import './bootstrap.esm.min.js';  
+// import './bootstrap.min.js';       
+
+
+
+
